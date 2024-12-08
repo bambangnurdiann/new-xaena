1d4ba077d69fd7725838afd1c0cd107d31615f28
bambangnurdiann <109570315+bambangnurdiann@users.noreply.github.com>
2024-12-08 18:52:11 +0700

    Update route.ts

--- app/api/auth/check/route.ts
+++ app/api/auth/check/route.ts
@@ -1,29 +1,11 @@
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
-import clientPromise from '@/lib/mongodb'
 
 export async function GET() {
   const cookieStore = cookies()
-  const sessionToken = cookieStore.get('session_token')?.value
+  const isAuthenticated = cookieStore.get('auth')?.value === 'true'
+  const userId = cookieStore.get('user')?.value
 
-  if (!sessionToken) {
-    return NextResponse.json({ isAuthenticated: false })
-  }
-
-  try {
-    const client = await clientPromise
-    const db = client.db("xaena_db")
-    const usersCollection = db.collection('login_user')
-
-    const user = await usersCollection.findOne({ sessionToken })
-
-    if (user) {
-      return NextResponse.json({ isAuthenticated: true, userId: user.userId })
-    } else {
-      return NextResponse.json({ isAuthenticated: false })
-    }
-  } catch (error) {
-    console.error('Error checking authentication:', error)
-    return NextResponse.json({ error: 'Internal server error' }, { status: 500 })
-  }
+  return NextResponse.json({ isAuthenticated, userId })
 }
+
