060c433dbd4ab55ed499e82bb8f44114535f8235
bambangnurdiann <109570315+bambangnurdiann@users.noreply.github.com>
2024-12-18 08:19:13 +0700

    Update route.ts

--- app/api/checkLoggedInUsers/route.ts
+++ app/api/checkLoggedInUsers/route.ts
@@ -7,7 +7,7 @@ import clientPromise from '@/lib/mongodb';
 export async function GET() {
   try {
     const client = await clientPromise;
-    const db = client.db('xaena_debug');
+    const db = client.db('xaena_db');
     const usersCollection = db.collection('login_user');
 
     const cookieStore = cookies();
