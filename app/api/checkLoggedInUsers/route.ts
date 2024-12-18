ba020045749f348078e53f2154fc4fb59ec550e8
bambangnurdiann <109570315+bambangnurdiann@users.noreply.github.com>
2024-12-18 08:18:40 +0700

    Update route.ts

--- app/api/checkLoggedInUsers/route.ts
+++ app/api/checkLoggedInUsers/route.ts
@@ -7,7 +7,7 @@ import clientPromise from '@/lib/mongodb';
 export async function GET() {
   try {
     const client = await clientPromise;
-    const db = client.db('xaena_db');
+    const db = client.db('xaena_debug');
     const usersCollection = db.collection('login_user');
 
     const cookieStore = cookies();
@@ -17,9 +17,22 @@ export async function GET() {
       return NextResponse.json({ loggedInUsers: [] });
     }
 
+    // Cari pengguna berdasarkan sessionToken dan loggedIn
     const loggedInUsers = await usersCollection
       .find({ sessionToken, loggedIn: true })
       .toArray();
+
+    // Reset isWorking jika pengguna logout sebelumnya
+    await Promise.all(
+      loggedInUsers.map(async (user) => {
+        if (!user.sessionToken) {
+          await usersCollection.updateOne(
+            { username: user.username },
+            { $set: { isWorking: false } }
+          );
+        }
+      })
+    );
 
     console.log('Logged in users:', loggedInUsers);
 
@@ -31,4 +44,3 @@ export async function GET() {
     return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
   }
 }
-
