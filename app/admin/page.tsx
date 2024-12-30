43ce415c4a1de59e534237a575c1a7eafb7d7446
bambangnurdiann <109570315+bambangnurdiann@users.noreply.github.com>
2024-12-30 13:52:29 +0700

    Add files via upload

---
+++ app/admin/page.tsx
@@ -0,0 +1,10 @@
+import { TeamLeaderDashboard } from '@/components/TeamLeaderDashboard'
+
+export default function AdminPage() {
+  return (
+    <div className="container mx-auto p-4">
+      <TeamLeaderDashboard />
+    </div>
+  )
+}
+
